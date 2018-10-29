8ce4b86b3840fe35813f490e0f1bcd9fb0468a45
jens <j.dahlmanns@tevaris.de>
2018-10-29 16:45:22 +0100

    init

---
+++ src/sample/Controller.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Controller {
+
+  @FXML
+  public void doStep1(ActionEvent actionEvent) {
+    Stage stage = new Stage(StageStyle.DECORATED);
+    stage.setTitle("Title");
+    stage.initModality(Modality.NONE);
+
+    Button b = new Button("Hello");
+    b.setMinWidth(200.0);
+    Scene scene = new Scene(new HBox(b));
+    stage.setScene(scene);
+
+    stage.show();
+  }
+
+  @FXML
+  public void doStep2(ActionEvent actionEvent) {
+    Alert alert = new Alert(AlertType.INFORMATION);
+    alert.setHeaderText("INFO");
+    alert.show();
+  }
+}
